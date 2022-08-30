fa254f2752937d2bf082e34009eb218a9d9a56df
Ashley Wulber <ashley@system76.com>
2022-08-30 18:54:22 -0400

    feat: template for relm4 applets

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-mod _components;
 mod components;
 #[rustfmt::skip]
 mod config;
 mod localize;
 
-use relm4::{adw::{gio, glib}, RelmApp};
+use config::APP_ID;
+use log::info;
+use relm4::{adw::{gio, glib, gtk, gdk, builders::ApplicationBuilder}, RelmApp};
 
-use _components::{ExampleApplication, app_model::AppModel};
 use localize::localize;
+
+use crate::{config::{VERSION, PROFILE}, components::example::Example};
 
 fn main() {
     let _monitors = libcosmic::init();
 
     // Initialize logger
     pretty_env_logger::init();
+    info!("Cosmic Application Template ({})", APP_ID);
+    info!("Version: {} ({})", VERSION, PROFILE);
+    
+    let provider = gtk::CssProvider::new();
+    provider.load_from_resource("/com/system76/CosmicApplicationTemplate/style.css");
+    if let Some(display) = gdk::Display::default() {
+        gtk::StyleContext::add_provider_for_display(
+            &display,
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+    }
 
     // Prepare i18n
     localize();
@@ -22,8 +36,10 @@ fn main() {
 
     glib::set_application_name(&fl!("app-name"));
 
-    let app = ExampleApplication::new();
-    let relm_app = RelmApp::with_app(app.clone());
-    relm_app.run::<AppModel>(());
-    // app.run();
+    let app = ApplicationBuilder::new()
+    .resource_base_path("/com/system76/CosmicApplicationTemplate/")
+    .flags(gio::ApplicationFlags::empty())
+    .build();
+    let relm_app = RelmApp::with_app(app);
+    relm_app.run::<Example>(0);
 }
